f87a53ca127e540c00839a0310aa9895eae16dbd
Sofia Katzman <katzmansof@gmail.com>
2023-01-23 22:29:48 -0500

    6/7 completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+
+
+const employee = {
+    name: "Sofia" , 
+    streetAddress: "12 Broadway" };
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    return {
+    ...employee, [key] : value,}
+    }
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+
+
+function deleteFromEmployeeByKey (employee, key) {
+       const newEmployee = {...employee[key]};
+       delete newEmployee.key;        
+       return newEmployee;
+}
+
+
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    const originalEmployee = {employee, key};
+    delete employee.firstname;
+    return originalEmployee; 
+}
+
+
+
